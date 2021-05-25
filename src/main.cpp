d4d63a75164cb0f6e4918a73d2a333836c8b8ccb
Ivan San Martin Fernandez <ismf1@alu.ua.es>
2021-05-25 19:18:20 +0200

    Calculos de tiempos

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,38 @@
 #include <iostream> 
 #include <string>
+#include <list>
+#include <sys/resource.h>
 #include "buscador.h"
-#include "indexadorHash.h"
-
 using namespace std;
 
+double getcputime(void) {  
+    struct timeval tim;         
+    struct rusage ru;         
+    getrusage(RUSAGE_SELF, &ru);         
+    tim=ru.ru_utime;         
+    double t=(double)tim.tv_sec + (double)tim.tv_usec / 1000000.0;         
+    tim=ru.ru_stime;         
+    t+=(double)tim.tv_sec + (double)tim.tv_usec / 1000000.0;         
+    return t;  
+} 
 
-main() {
-    IndexadorHash b("./StopWordsEspanyol.txt", ". ,:", false, false, "./indicePrueba", 0, false, false);
-
-    b.Indexar("./listaFicheros_corto.txt");
-    b.GuardarIndexacion();
-
-    Buscador a("./indicePrueba", 0);
-    string preg;
-    double kk1; double kb;
-
-    a.IndexarPregunta("pal1 pal4 pal2 pal3 pal1. pal2, pal4 pal2");
-
-    if(a.Buscar(1))
-        a.ImprimirResultadoBusqueda(1);
-
-    a.CambiarFormulaSimilitud(1);
-
-    if(a.Buscar(1))
-        a.ImprimirResultadoBusqueda(1);
-
-}
+main() { 
+    double aa=getcputime();
+    IndexadorHash b("./StopWordsEspanyol.txt",   ".   ,:",   false,   false, "./indicePruebaEspanyol", 0, false, false); 
+    b.Indexar("ficherosTimes.txt"); 
+    b.GuardarIndexacion(); 
+    Buscador a("./indicePruebaEspanyol", 0); 
+    a.IndexarPregunta("KENNEDY  ADMINISTRATION  PRESSURE  ON  NGO  DINH  DIEM  TO STOP SUPPRESSING THE BUDDHISTS . "); 
+    a.Buscar(423); 
+    a.ImprimirResultadoBusqueda(423); 
+    double bb=getcputime()-aa; 
+    
+    time_t inicioB, finB; 
+    time(&inicioB); 
+    double aaB=getcputime(); 
+    a.Buscar("./CorpusTime/Preguntas/",   423,   1, 83); 
+    a.ImprimirResultadoBusqueda(423); 
+    double bbB=getcputime()-aaB; 
+    cout << "\nHa tardado primero " << bb << " segundos\n\n"; 
+    cout << "\nHa tardado segundo " << bbB << " segundos\n\n"; 
+} 
